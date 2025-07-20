a1e03f35ddbcb2c56a180dbcd9efed935705edb5
pavithra8867 <pavithra8867282739@gmail.com>
2025-07-20 22:08:29 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+const studentData = {
+  "pavithra@example.com": {
+    name: "Pavithra",
+    email: "pavithra@example.com",
+    mobile: "9876543210",
+    domain: "Internship Domain",
+    college: "Vaults of codes",
+    start: "01 July 2025",
+    duration: "1 Month",
+    certificate: "https://github.com/pavithra8867/pavithra-certificate/raw/main/pavithra_certificate.pdf"
+  }
+};
